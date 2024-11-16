1403be5d33702082609a3e1efa6bbc1d5e846450
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-11-16 11:57:52 +0100

    feature: implement cors

--- dashboard_backend/src/main.ts
+++ dashboard_backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ["https://job-application-dashboard.vercel.app/"], // or your frontend domain
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
